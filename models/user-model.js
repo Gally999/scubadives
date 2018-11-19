b830208e654de727b5437067f54eeac0d1aae856
Fareaha Isnin <fareaha09@gmail.com>
2018-11-19 16:29:08 +0100

    updated

--- models/user-model.js
+++ models/user-model.js
@@ -49,7 +49,7 @@ const userSchema = new Schema({
     },
     speciality:{
         type: [String],
-        enum:["Altitude Diver", "Boat Diver", "Fish Identification", "Drift Diver", "Peak Performance Buoyancy", "Night Diver", "Wreck Diver", "Dry Suit Diver", "Advanced Rebreather Diver", "Deep Diver", "Cave Diver" , "Search and Recovery Diver", "Emergency First Response Provider", "Ice Diver"],
+        enum:["None", "Altitude Diver", "Boat Diver", "Fish Identification", "Drift Diver", "Peak Performance Buoyancy", "Night Diver", "Wreck Diver", "Dry Suit Diver", "Advanced Rebreather Diver", "Deep Diver", "Cave Diver" , "Search and Recovery Diver", "Emergency First Response Provider", "Ice Diver"],
         default: "None",
     },
     role:{
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const User = require('../models/user-model');
 const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
+  //req.user comes from passport-setup.
+  if(req.user){
+    console.log("logged in", req.user);
+  }else {
+    console.log("not logged in", req.user);
+  }
+  res.locals.currentUser = req.user;
   res.render('index');
 });
 
