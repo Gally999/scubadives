f83fd2b526709ff98a8900cb7460eac8315701f0
Fareaha Isnin <fareaha09@gmail.com>
2018-11-19 11:28:26 +0100

    user model added

---
+++ models/user-model.js
@@ -0,0 +1,77 @@
+const mongoose=require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: {
+        type:String,
+        required: true,
+        minlength: 2,
+    },
+    lastName: {
+        type:String,
+        required: true,
+        minlength: 2,
+    },
+    nationality: {
+        type:String,
+        required: true,
+        minlength: 2,
+    },
+    email: {
+        type:String,
+        required:true,
+        unique: true,
+        match: /^.+@.+\..+$/,
+    },
+    encryptedPassword: {
+        type:String,
+    },
+    organization:{
+        type: String,
+        enum:["PADI", "SSI", "SDI"],
+        required: true,
+    },
+    certNb:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    mainCert:{
+        type: String,
+        enum:["Scuba Diver", "Open Water", "Adventure Diver", "Advanced Open Water", "Rescue Diver", "Master Scuba Diver"],
+        required: true,
+    },
+    secCert:{
+        type: String,
+        enum:["Dive Master", "Open Water Scuba Instructor",]
+    },
+    speciality:{
+        type: String,
+        enum:["Altitude Diver", "Boat Diver", "Fish Identification", "Drift Diver", "Peak Performance Buoyancy", "Night Diver", "Wreck Diver", "Dry Suit Diver", "Advanced Rebreather Diver", "Deep Diver", "Cave Diver, Search and Recovery Diver", "Emergency First Response Provider"],
+    },
+    role:{
+        type: String,
+        enum: ["diver", "shopowner", "admin"],
+        required: true,
+        default: "diver",
+    },
+    isShopowner:{
+        type:boolean,
+        required: true,
+    }
+},{
+    timestamps:true,
+});
+
+//booleans for Admin users
+// use the regular annoymous function rather than the arrow annoymous function
+userSchema.virtual('isShopowner').get(function () {
+    return this.role === 'shopowner';
+});
+
+userSchema.virtual('isAdmin').get(function () {
+    return this.role === 'admin';
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
