1528eddfa5cc7eeaf4cfc22ed2890c5914f61161
Cecile Daguin <gally999@gmail.com>
2018-11-19 14:24:49 +0100

    add login route + login-process

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -9,4 +9,28 @@ router.get('/signup', (req,res,next) =>{
     res.render('auth-views/signup-form')
 });
 
+router.get("/login", (req, res, next) => {
+    res.render("auth-route/login-form.hbs");
+});
+
+router.post("/login-process", (req, res, next) => {
+    const { email, originalPassword } = req.body;
+
+    User.findOne({ email: { $eq: email } })
+        .then(userDoc => {
+            if (!userDoc) {
+                req.flash("error", "Please create an account first");
+                res.redirect("/signup");
+            }
+            const { encryptedPassword } = userDoc;
+            if(bcrypt.compareSync(originalPassword, encryptedPassword)) {
+                res.redirect("/");
+            } else {
+                req.flash("error", "Wrong password, please try again!");
+                res.redirect("/login");
+            }
+        })
+        .catch(err => next(err));
+});
+
 module.exports = router;
