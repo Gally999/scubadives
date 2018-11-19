36c8e323689be9bc44e78772883db517b0f242f0
Cecile Daguin <gally999@gmail.com>
2018-11-19 11:49:15 +0100

    add divestite-model

---
+++ models/divesite-model.js
@@ -0,0 +1,41 @@
+const mongoose=require("mongoose");
+const Schema = mongoose.Schema;
+
+const divesiteSchema = new Schema({
+  name: {
+      type:String,
+      required: true,
+  },
+  city: { 
+    type: String, 
+    required: true 
+  },
+  country: { 
+    type: String, 
+    required: true, 
+    minlength: 2 
+  }, 
+  rating: { 
+    type: Number, 
+    min: 1,
+    max: 5, 
+  }, 
+  reviews: [{
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required:true,
+    },
+    comments: {
+      type:String,
+      required:true,
+      maxlength: 200,
+    },
+  }],
+},{
+  timestamps:true,
+});
+
+
+const Divesite = mongoose.model("Divesite", divesiteSchema);
+module.exports = Divesite;
