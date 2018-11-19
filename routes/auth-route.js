4ce5625cb20d4a587fe2ab100979bad66ccfe34f
Fareaha Isnin <fareaha09@gmail.com>
2018-11-19 12:30:08 +0100

    adding routes

---
+++ routes/auth-route.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const passport = require('passport');
+const User = require("../models/user-model.js");
+const router = express.Router();
+
+module.exports = router;
