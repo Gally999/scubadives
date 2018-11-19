c3b0e5f4dae00c2970f52b839c77b4b299660bee
Fareaha Isnin <fareaha09@gmail.com>
2018-11-19 14:26:25 +0100

    login

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -9,4 +9,30 @@ router.get('/signup', (req,res,next) =>{
     res.render('auth-views/signup-form')
 });
 
+router.post('/adduser', (req,res,next) =>{
+    const { firstName, lastName, nationality, email, originalPassword, organization, certNb, mainCert, secCert, speciality, isShopOwner} = req.body;
+    if (!originalPassword || !firstName || !lastName|| !nationality || !email|| !organization || !certNb || !mainCert || !isShopOwner|| originalPassword.match(/[0-9]/) === null){
+        req.flash("error", "Fields cannot be blank and Password must contain a number");
+        res.redirect('/signup');
+        return;
+    }
+    User.findOne({email: {$eq: email}})
+    .then(userDoc =>{
+        if (userDoc){
+            req.flash("error", "Email is already in the Database");
+            res.redirect('/login');
+            return;
+        }
+
+        const encryptedPassword = bcrypt.hashSync(originalPassword, 10);
+        User.create({firstName, lastName, nationality, email, encryptedPassword, organization, certNb, mainCert, secCert, speciality, isShopOwner})
+        .then(userDoc =>{
+            req.flash("success", "Created Successfully");
+            res.redirect('/');
+        })
+        .catch(err =>next(err))
+    })
+    .catch(err =>next(err))
+});
+
 module.exports = router;
